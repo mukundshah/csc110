b9fe0f77c665c7bbc0d8be21a254bfe24408ab64
Mukund Shah <mukundshah058@gmail.com>
2021-05-14 08:41:52 +0545

    Lab 1 (May 13, 2021)

---
+++ Lab/Lab_1_May_13.c
@@ -0,0 +1,26 @@
+/*
+Lab 1: Write a program that reads a number and tests whether it is multiple of 7 or not.
+Date: May 13, 2021
+*/
+
+#include <stdio.h>
+int main(){
+    int n, r;
+    printf("Enter any number (n): ");
+    scanf("%d", &n);
+    r= n%7;
+    if(r==0)
+        printf("The numer is mutiple of 7.");
+    else
+        printf("The number is not multiple of 7.");
+    return 0;
+}
+
+/*
+----OUTPUT----
+Enter any number (n): 34
+The number is not multiple of 7.
+
+Enter any number (n): 21
+The number is multiple of 7.
+*/

e15ff4375b586bd967125ac01c0618706f91d715
Mukund Shah <mukundshah058@gmail.com>
2021-05-14 08:47:21 +0545

    Lab 3 (May 13, 2021)

---
+++ Lab/Lab_3_May_13.c
@@ -0,0 +1,27 @@
+/*
+Lab 2: Write a C program that reads a two digit number and finds its sum of digits.
+For example, if number is 76 then its digit sum is 13.
+Date: May 13, 2021
+*/
+
+#include <stdio.h>
+
+int main(){
+    int n, s, d1, d2;
+    printf("Enter any two digit number: ");
+    scanf("%d", &n);
+    d1 = n%10;
+    d2 = n/10;
+    s = d1 + d2;
+    printf("The sum of digits = %d", s);
+    return 0;
+}
+
+/*
+----OUTPUT----
+Enter any two digit number: 76
+The sum of digits = 13
+
+Enter any two digit number: 97
+The sum of digits = 16
+*/
